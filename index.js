01c6a6d7db9b9ccd491438c3acb0e1b6b9a750b4
Parag <paragbokde51@gmail.com>
2021-12-14 18:36:36 +0530

    added production port

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const metaData = require('./routes/urlMetaData.js')
 
 const app = express()
 
+const port = process.env.PORT || 4000
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/urlMetaData', metaData)
 
-app.listen(4000, () => console.log('Server running at http://localhost:4000'))
+app.listen(port, () => console.log('Server running at http://localhost:4000'))
