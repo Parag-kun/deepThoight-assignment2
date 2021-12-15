c64017c99b13f2079b608a1811c64a07b864c602
Parag <paragbokde51@gmail.com>
2021-12-15 13:32:40 +0530

    fixed the issue

--- routes/urlMetaData.js
+++ routes/urlMetaData.js
@@ -1,22 +1,31 @@
 const router = require("express").Router()
 const request = require('request-promise')
+const cheerio = require('cheerio')
 const { parse } = require('node-html-parser')
 
 const trim = str => str.slice(str.indexOf('h'), str.length - 1)
 
 router.post('/', async (req, res) => {
     const metaData = {}
-    const response = await request(req.body.uri)
-    const root = parse(response.toString())
+    const $ = await request({
+        uri: req.body.uri,
+        headers: {
+            'Content-Type': 'text/html',
+            'User-Agent': 'Request-Promise'
+        },
+        transform: body => cheerio.load(body),
+        gzip: true
+    })
+    const root = parse($.html())
 
     root.querySelectorAll('meta[property^="og:"]').forEach(element => {
         metaData[element._attrs.property] = element._attrs.content
     })
 
     if (Object.keys(metaData).length === 0) {
-        metaData.title = root.querySelector('title').textContent
-        metaData.description = root.querySelector('meta[name="description"]')._attrs.content
-        metaData.images = root.querySelectorAll('img[src^="https://"]').map(({ rawAttrs }) => trim(rawAttrs.slice(rawAttrs.indexOf('src=')).split(' ')[0].split('\n')[0]))
+        metaData.title = root.querySelector('title')?.textContent
+        metaData.description = root.querySelector('meta[name="description"]')?._attrs.content
+        metaData.images = root.querySelectorAll('img[src^="https://"]')?.map(({ rawAttrs }) => trim(rawAttrs.slice(rawAttrs.indexOf('src=')).split(' ')[0].split('\n')[0]))
     }
 
     res.json(metaData)
